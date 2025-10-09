8a5195e32ebd7bc15cbeccab9c9b98b1976acb26
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-10-10 01:27:06 +0500

    configured multer and created DiskStorage using multer for incoming files

---
+++ Backend/utils/multerStorage.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", "..", "Uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileName = file.originalname?.split('.')[0]
+    console.log('file is',file)
+    cb(
+      null,
+      fileName + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+module.exports = uploads;
